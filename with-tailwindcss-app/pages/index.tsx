fb0111e6f4783ace262e11132e8c2f67298706fd
Dian <guodian44@gmail.com>
2023-01-17 22:01:53 +0800

    disabled, color change, render elements

--- with-tailwindcss-app/pages/index.tsx
+++ with-tailwindcss-app/pages/index.tsx
@@ -1,8 +1,38 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import { useState } from "react";
 
 const Home: NextPage = () => {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState("");
+
+  const handleClick = async () => {
+    setData("");
+    setLoading(true);
+    const response = { something: "value" };
+    await setTimeout(() => {
+      console.log("delay fetching data");
+      setData(response.something);
+      setLoading(false);
+    }, 1000);
+  };
+
+  return (
+    <div>
+      {loading && <div className="spinner">Loading...</div>}
+      <button
+        className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full ${
+          loading ? "bg-gray-300 hover:bg-gray-300" : ""
+        }`}
+        onClick={handleClick}
+        disabled={loading}
+      >
+        Fetch Data
+      </button>
+      {data && <div>{data}</div>}
+    </div>
+  );
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>

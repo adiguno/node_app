3d9a79940e7f4959b6ab1c48f1fc6245e6563a7a
Dian <guodian44@gmail.com>
2023-01-24 22:20:09 +0800

    add comments if to change resp header for endpoint

--- with-tailwindcss-app/next.config.js
+++ with-tailwindcss-app/next.config.js
@@ -2,4 +2,25 @@
 module.exports = {
   reactStrictMode: true,
   distDir: "build",
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/events",
+  //       headers: [
+  //         {
+  //           key: "Content-Type",
+  //           value: "text/event-stream",
+  //         },
+  //         {
+  //           key: "Cache-Control",
+  //           value: "no-cache",
+  //         },
+  //         {
+  //           key: "Connection",
+  //           value: "keep-alive",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };

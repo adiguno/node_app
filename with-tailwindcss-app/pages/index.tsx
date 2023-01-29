267c9140c98f1ec6f47ccd606605d0c556c65fbb
Dian <guodian44@gmail.com>
2023-01-29 23:35:01 +0800

    basic game

--- with-tailwindcss-app/pages/index.tsx
+++ with-tailwindcss-app/pages/index.tsx
@@ -1,7 +1,111 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { EventHandler, useState } from "react";
+type SquareType = {
+  value: string;
+  onSquareClick: any;
+};
+function Square(props: SquareType) {
+  return (
+    <button className="border-2 h-36 w-36" onClick={props.onSquareClick}>
+      {props.value}
+      {/* {value} */}
+    </button>
+  );
+}
 
+export function Board(props: { xIsNext: boolean; squares: string[]; onPlay: Function }) {
+  // state lifed from square
+  // when this state/prop to the child change, it and the child re-renders
+  // const [squares, setSquares] = useState(Array(9).fill(""));
+  // const [xIsNext, setXIsNext] = useState(true);
+
+  function handleClick(i: number) {
+    if (props.squares[i] || calculateWinner(props.squares)) return;
+    const nextSquares = props.squares.slice();
+    // nextSquares[i] = "X";
+    if (props.xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    // setSquares(nextSquares);
+    // setXIsNext(!xIsNext);
+    props.onPlay(nextSquares);
+  }
+
+  const winner = calculateWinner(props.squares);
+  console.log("feels like this is run every state change");
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (props.xIsNext ? "X" : "O");
+  }
+
+  return (
+    <>
+      <div className="status">{status}</div>
+
+      <div className="board-row">
+        <Square value={props.squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={props.squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={props.squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={props.squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={props.squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={props.squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={props.squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={props.squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={props.squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+}
+export function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares: string[]) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove: number) {
+    // TODO
+  }
+  const moves = history.map((_, move) => {
+    let description;
+    if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+    return (
+      <li>
+        <button className="border-2" onClick={() => jumpTo(move)}>
+          {description}
+        </button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
@@ -11,7 +115,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">Welcome to the jungle</h1>
+        <Game />
       </main>
 
       <footer className="flex h-24 w-full items-center justify-center border-t">footer</footer>
@@ -20,3 +124,23 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+function calculateWinner(squares: any[]) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}

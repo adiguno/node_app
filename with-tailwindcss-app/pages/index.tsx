339ecc1af147afe4925d9eec8a2546763ab9bb28
Dian <guodian44@gmail.com>
2023-02-03 13:47:23 +0800

    finished tutorial

--- with-tailwindcss-app/pages/index.tsx
+++ with-tailwindcss-app/pages/index.tsx
@@ -67,18 +67,23 @@ export function Board(props: { xIsNext: boolean; squares: string[]; onPlay: Func
   );
 }
 export function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  // const currentSquares = history[history.length - 1];
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares: string[]) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    // setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
 
   function jumpTo(nextMove: number) {
-    // TODO
+    setCurrentMove(nextMove);
   }
+
   const moves = history.map((_, move) => {
     let description;
     if (move > 0) {
@@ -87,7 +92,7 @@ export function Game() {
       description = "Go to game start";
     }
     return (
-      <li>
+      <li key={move}>
         <button className="border-2" onClick={() => jumpTo(move)}>
           {description}
         </button>

eeb0038f46f202e8ce890dbd21b72c6155e1a608
Dian <guodian44@gmail.com>
2023-02-04 22:47:11 +0800

    Add openai display streaming response

---
+++ with-tailwindcss-app/pages/api/generate.ts
@@ -0,0 +1,42 @@
+import { OpenAIStream } from "../../utils/OpenAIStream";
+
+if (!process.env.OPENAI_API_KEY) {
+  throw new Error("Missing env var from OpenAI");
+}
+
+export const config = {
+  runtime: "edge",
+};
+
+const handler = async (req: Request): Promise<Response> => {
+  // const { prompt } = req.body as {
+  //   prompt?: string;
+  // };
+
+  const { prompt } = (await req.json()) as {
+    prompt?: string;
+  };
+  console.log(`---got prompt---`);
+  console.log(prompt);
+
+  if (!prompt) {
+    return new Response("No prompt in the request", { status: 400 });
+  }
+
+  const payload = {
+    model: "text-davinci-003",
+    prompt,
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 50,
+    stream: true,
+    n: 1,
+  };
+
+  const stream = await OpenAIStream(payload);
+  return new Response(stream);
+};
+
+export default handler;
--- with-tailwindcss-app/pages/index.tsx
+++ with-tailwindcss-app/pages/index.tsx
@@ -1,20 +1,92 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import { useState } from "react";
 
 const Home: NextPage = () => {
+  const [bio, setBio] = useState("");
+  const [generatedBios, setGeneratedBios] = useState<String>("");
+
+  const prompt = `Generate a funny twitter bio with this context: ${bio}`;
+
+  const generateBio = async (e: any) => {
+    e.preventDefault();
+    setGeneratedBios("");
+
+    console.log("---- prompt =====");
+    console.log(prompt);
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt,
+      }),
+    });
+    console.log("returned response:");
+    console.log(response);
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    const data = response.body;
+    console.log("readable stream data:");
+    console.log(data);
+    if (!data) {
+      return;
+    }
+
+    const reader = data.getReader(); // web api
+    const decoder = new TextDecoder();
+    let done = false;
+
+    while (!done) {
+      const { value, done: doneReading } = await reader.read();
+
+      console.log(`----------got stream data----------`);
+      console.log(value);
+      done = doneReading;
+      const chunkValue = decoder.decode(value);
+      setGeneratedBios((prev) => prev + chunkValue);
+    }
+  };
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">Welcome to the jungle</h1>
-      </main>
+        <textarea
+          value={bio}
+          onChange={(e) => setBio(e.target.value)}
+          rows={4}
+          className="w-full rounded-md bg-slate-200 border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
+          placeholder={"e.g. Senior Engineer @vercel. Tweeting about web dev & AI."}
+        />
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">footer</footer>
+        <button
+          className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
+          onClick={(e) => generateBio(e)}
+        >
+          Generate your bio &rarr;
+        </button>
+
+        <div className="bg-slate-300">
+          <div>
+            <h2 className="text-red-900">Your generated bios</h2>
+          </div>
+          <div className="">
+            {generatedBios
+              .substring(generatedBios.indexOf("1") + 3)
+              .split("2.")
+              .map((generatedBio: any) => {
+                return (
+                  <div className="bg-slate-600" key={generatedBio}>
+                    <p>{generatedBio}</p>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
